fb31c5bda09a2fc3bd6d2bf399f58dd3a568b412
khalid <khalid.douch@students.fhnw.ch>
2025-10-31 13:44:23 +0100

    Evengenrator v1.0

--- src/main/java/event_generator/EventGenerator.java
+++ src/main/java/event_generator/EventGenerator.java
@@ -1,8 +1,7 @@
 package event_generator;
 
 import java.time.Duration;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -14,7 +13,7 @@ public class EventGenerator {
     public static void main(String[] args) {
 
         /// !!!!!!!!!!!!!!!!!!!!!! Name of group:
-        String group = "group1"; //// CHANGE ME!!!!
+        String group = "group1";
 
         // Name of the Kafka topic to publish the events to (please keep the group name
         // as prefix to prevent conflicts with other groups)
@@ -38,28 +37,45 @@ public class EventGenerator {
         // - The Event ID is the product ID
         // - The Event content/value is the amount that was sold
 
-        // Liste der Produktkategorien
-        String[] categories = {
-                "Notebook", "PC", "Camera", "Tablet", "Audio", "Peripherals", "Display"
-        };
 
+        // Liste mit allen Produktkategorien
+        Map<String, List<Integer>> categories = new HashMap<>();
+
+        categories.put("Monitor", new ArrayList<>(Arrays.asList(101, 102)));
+        categories.put("Tastatur", new ArrayList<>(Arrays.asList(201, 202)));
+        categories.put("Notebook", new ArrayList<>(Arrays.asList(303, 308, 410)));
+        categories.put("Audio", new ArrayList<>(Arrays.asList(304, 305, 306)));
+        categories.put("Camera", new ArrayList<>(Arrays.asList(307)));
+        categories.put("Tablet", new ArrayList<>(Arrays.asList(302)));
+        categories.put("PC", new ArrayList<>(Arrays.asList(898, 899, 888)));
+
+        //Objekt für Random-Berechnungen
         Random random = new Random();
 
-        for (;;) { // endless loop
+        for (; ; ) { // endless loop
 
             // zufällige Kategorie auswählen
-            String category = categories[random.nextInt(categories.length)];
+            List<String> kategorienNamen = new ArrayList<>(categories.keySet());
+            String zufallsKategorie = kategorienNamen.get(random.nextInt(kategorienNamen.size()));
 
-            // zufälligen Umsatz zwischen 100 und 5000 generieren
-            //int revenue = 100 + random.nextInt(4901);
-            int product_id = 1 + random.nextInt(9);
+
+            //Zufällige Produkt-ID aus dieser Kategorie auswählen
+            List<Integer> produkte = categories.get(zufallsKategorie);
+            int zufallsProdukt = produkte.get(random.nextInt(produkte.size()));
+
+
+            //Zufällige Bestellmenge von 1 bis 3
             int amount = 1 + random.nextInt(3);
 
-            //String key = category; // Kategorie als Schlüssel
-            //String value = String.valueOf(revenue); // Umsatz als String-Wert
+            // Kategorienamen als Schlüssel nehmen
+            String key = String.valueOf(zufallsProdukt);
 
-            String key = String.valueOf(product_id); // Kategorie als Schlüssel
-            String value = String.valueOf(amount); // Umsatz als String-Wert
+
+            // Als Value wird ein JSON-Format genommen
+            String value = String.format(
+                    "{\"product_id\": %d, \"amount\": %d, \"category\": \"%s\"}",
+                    zufallsProdukt, amount, zufallsKategorie
+            );
 
             // now create and send the event to Kafka
             ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
